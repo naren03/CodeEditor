dabfadf2802791c02548139edad7e69639028d1b
Narendra <naren3001singh@gmail.com>
2021-12-08 18:21:12 +0530

    FileSaver Added

--- index.js
+++ index.js
@@ -2,13 +2,109 @@ const htmlCode = document.querySelector('.html-code');
 const cssCode = document.querySelector('.css-code');
 const jsCode = document.querySelector('.js-code');
 const output = document.querySelector('.output-display');
+let htmlData, cssData, jsData;
+
+//HTML EDITOR
+let editor1 = CodeMirror.fromTextArea(htmlCode, {
+	mode: 'text/html',
+	theme: 'dracula',
+	lineNumbers: true,
+	autoCloseTags: true,
+	lineWrapping: true,
+});
+// editor1.setSize('550', '250');
 
 function run() {
 	output.contentDocument.body.innerHTML =
-		htmlCode.value + '<style>' + cssCode.value + '</style>';
-	output.contentWindow.eval(jsCode.value);
+		htmlData + '<style>' + cssData + '</style>';
+	output.contentWindow.eval(jsData);
 }
 
-htmlCode.addEventListener('keyup', run);
-cssCode.addEventListener('keyup', run);
-jsCode.addEventListener('keyup', run);
+//When editors value changes
+CodeMirror.on(editor1, 'change', () => {
+	htmlData = editor1.getValue();
+
+	//call the main function
+	run();
+});
+
+// htmlCode.addEventListener('keyup', run);
+// cssCode.addEventListener('keyup', run);
+// jsCode.addEventListener('keyup', run);
+
+//CSS EDITOR
+let editor2 = CodeMirror.fromTextArea(cssCode, {
+	mode: 'css',
+	theme: 'dracula',
+	lineNumbers: true,
+	autoCloseTags: true,
+	lineWrapping: true,
+});
+// editor2.setSize('550', '250');
+
+//When editors value changes
+CodeMirror.on(editor2, 'change', () => {
+	cssData = editor2.getValue();
+
+	//call the main function
+	run();
+});
+
+//JS EDITOR
+let editor3 = CodeMirror.fromTextArea(jsCode, {
+	mode: 'javascript',
+	theme: 'dracula',
+	lineNumbers: true,
+	autoCloseTags: true,
+	lineWrapping: true,
+});
+// editor3.setSize('550', '250');
+
+//When editors value changes
+CodeMirror.on(editor3, 'change', () => {
+	jsData = editor3.getValue();
+
+	//call the main function
+	run();
+});
+
+//Download Functionality
+document.getElementById('download-btn').addEventListener('click', (e) => {
+	e.preventDefault();
+	//generate File
+	var text = htmlData;
+	var filename = 'index.html';
+
+	//create blob
+	var blob = new Blob([text], { type: 'text/plain;charset=utf-8' });
+
+	//genarte file download(saveas)
+	saveAs(blob, filename);
+
+	//generate File
+	text = cssData;
+	filename = 'styles.css';
+
+	//create blob
+	blob = new Blob([text], { type: 'text/plain;charset=utf-8' });
+
+	//genarte file download(saveas)
+	saveAs(blob, filename);
+
+	//generate File
+	text = jsData;
+	filename = 'index.js';
+
+	//create blob
+	blob = new Blob([text], { type: 'text/plain;charset=utf-8' });
+
+	//genarte file download(saveas)
+	saveAs(blob, filename);
+
+	//generate Canvas
+	var canvas = document.getElementById('my-output');
+	console.log(canvas);
+	canvas.toBlob(function (blob) {
+		saveAs(blob, 'output.png');
+	});
+});
